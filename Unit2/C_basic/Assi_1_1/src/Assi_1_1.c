33b5f7f9434a726cae693e0b725409129f627ed9
Yomna <yomnabassem7@gmail.com>
2023-06-23 13:08:40 +0300

    add assigment

--- Unit2/C_basic/Assi_1_1/src/Assi_1_1.c
+++ Unit2/C_basic/Assi_1_1/src/Assi_1_1.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : Assi_1_1.c
- Author      : 
+ Author      : Yomna Bassem
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -12,6 +12,6 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+printf("C programming")	;
+	return;
 }
