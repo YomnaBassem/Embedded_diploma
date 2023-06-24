fcfd80b8f9e977bd686ebad900807a5603d60334
Yomna <yomnabassem7@gmail.com>
2023-06-25 00:05:05 +0300

    Add Assi_2_5

---
+++ Unit2/C_basic/Assi_2_5/src/Assi_2_5.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : Assi_2_5.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	char x;
+	printf("Enter a character :");
+	fflush(stdin); fflush(stdout);
+	scanf("%c",&x);
+	if ((x>'A') && (x<'Z')){
+		printf("%c is a character",x);
+	}
+	else{
+		printf("%c is not a character",x);
+	}
+}
