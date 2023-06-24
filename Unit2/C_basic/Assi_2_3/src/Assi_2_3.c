4c0b0b671e3b01bca581ae304dc57cba4f364565
Yomna <yomnabassem7@gmail.com>
2023-06-24 23:36:54 +0300

    Add Assi_2_4

---
+++ Unit2/C_basic/Assi_2_3/src/Assi_2_3.c
@@ -0,0 +1,35 @@
+/*
+ ============================================================================
+ Name        : Assi_2_3.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	float x,y,z;
+	printf("Enter three numbers :");
+	fflush(stdin); fflush(stdout);
+	scanf("%f %f %f",&x,&y,&z);
+	if(x>y){
+		if(x>z){
+			printf("Largest number=%f",x);
+		}
+		else{
+			printf("Largest number=%f",z);
+		}
+	}
+	else if (y>x){
+		if(y>z){
+			printf("Largest number=%f",y);
+		}
+		else{
+			printf("Largest number=%f",z);
+		}
+	}
+}
